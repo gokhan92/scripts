79d056f96e33f145d500c5dfb0843c91f5e2b474
gokhan92 <gokhansimsek92@gmail.com>
2021-03-27 10:57:44 -0300

    Added error handling

--- serverCheck.py
+++ serverCheck.py
@@ -3,17 +3,31 @@ import subprocess
 import sys
 
 # Ports are handled in ~/.ssh/config since we use OpenSSH
-#COMMAND="service sssd status"
+command="service sssd status"
 
 listUrl=["http://server1:port","http://server2:port","http://server3:port","http://server4:port","http://server5:port"]
 
 
-for i in listUrl:
-    resp=urllib.request.urlopen(i).getcode()
-    if resp == 200:
-        print (i+" Server status " + str(resp))
-    else:
-        print(i+" Check this server ")
+try:
+    for i in listURL:
+        resp=urllib.request.urlopen(i).getcode()
+    
+        if resp == 200:
+            print(i+" Server status "+str(resp))
+            ssh = subprocess.Popen(["ssh", "%s" % i, command],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+            result = ssh.stdout.readlines()
+            if result == []:
+                error = ssh.stderr.readlines()
+                print (sys.stderr, "ERROR: %s" % error)
+            else:
+                print (result)
+except:
+        print("An error occurred in the server " + i + " Please check it")
+    
+    
         
 #You can add following ssh connection to run the remote commands in the if else control flow
 """
